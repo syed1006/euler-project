6ea2d15bf8f7e7f3cbf7e5e99312838bb9d87852
syed1006 <syedsalman1006@gmail.com>
2026-06-25 19:40:33 +0530

    multiple of 3 or 5

---
+++ 0001-multiple-of-3-or-5/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	limit := 1000
+	sum := sumMultiples(3, limit) + sumMultiples(5, limit) - sumMultiples(15, limit)
+
+	fmt.Println(sum)
+}
+
+func sumMultiples(divisor, limit int) int {
+	k := (limit - 1) / divisor
+	return divisor * k * (k + 1) / 2
+}
