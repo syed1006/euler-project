9db9734ac27bb94dcecc31314fb5b635603bf437
syed1006 <syedsalman1006@gmail.com>
2026-06-26 11:46:38 +0530

    added one more optimised sollution

--- 0003-largest-prime-factor/main.go
+++ 0003-largest-prime-factor/main.go
@@ -4,6 +4,7 @@ import "fmt"
 
 func main() {
 	fmt.Println(findLargestPrimeFactor(600851475143, 2))
+	fmt.Println(largestPrimeFactor(600851475143))
 }
 
 func findLargestPrimeFactor(number int, primeDivisor int) int {
@@ -32,13 +33,41 @@ func findNextPrimeNumber(number int) int {
 
 func isPrime(number int) bool {
 
-	for i := 2; i < number; i++ {
+	if number < 2 {
+		return false
+	}
+
+	for i := 2; i*i < number; i++ {
 		if number%i == 0 {
 			return false
 		}
 	}
 
 	return true
+}
+
+func largestPrimeFactor(n int64) int64 {
+	var factor int64 = 2
+
+	for n%2 == 0 {
+		n /= 2
+	}
+
+	factor = 3
+
+	for factor*factor <= n {
+		if n%factor == 0 {
+			n /= factor
+		} else {
+			factor += 2
+		}
+	}
+
+	if n > 1 {
+		return n
+	}
+
+	return factor
 }
 
 // 6857
