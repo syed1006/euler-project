1ce0b424f9d472f382867ba3714fd576d73df6ce
syed1006 <syedsalman1006@gmail.com>
2026-06-26 11:17:26 +0530

    even fibonacci numbers

---
+++ 0002-even-fibonacci-numbers/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fibonacciSeries := []int{1, 2}
+
+	evenSum := generateFibonacci(1, 2, 4000000, &fibonacciSeries, 2)
+
+	fmt.Println(evenSum)
+}
+
+func generateFibonacci(firstTerm int, secondTerm int, limit int, series *[]int, sum int) int {
+	nextTerm := firstTerm + secondTerm
+
+	if nextTerm%2 == 0 {
+		sum += nextTerm
+	}
+
+	if nextTerm > limit {
+		return sum
+	}
+
+	*series = append(*series, nextTerm)
+
+	return generateFibonacci(secondTerm, nextTerm, limit, series, sum)
+}
